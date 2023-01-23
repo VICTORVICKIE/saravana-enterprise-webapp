2c8af6695d2105d1d3937c7cd210879e2c09aeda
VICTORVICKIE <s.vickie14@gmail.com>
2023-01-23 20:32:19 +0530

    Landing Page for Site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,1 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
-
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
