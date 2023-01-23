14733986a3b345fa073e97ec6d7bf05632eb509e
VICTORVICKIE <s.vickie14@gmail.com>
2023-01-23 20:29:31 +0530

    Added DaisyUI and Tailwind-Scrollbar-Hide Plugins

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
+const { default: themes } = require('daisyui/src/colors/themes')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [require('daisyui'), require('tailwind-scrollbar-hide')],
+	daisyui: {
+		styled: true,
+		themes: true,
+		base: true,
+		utils: true,
+		logs: true,
+		rtl: false,
+		prefix: '',
+		darkTheme: 'black',
+		themes: ['black', 'lofi']
+	}
 }
