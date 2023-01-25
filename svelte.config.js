1d5322548da63a0fa6e71f872137a8bb20e7cb5e
VICTORVICKIE <s.vickie14@gmail.com>
2023-01-25 10:08:33 +0530

    Added Alias for Components Directory

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,11 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$components: 'src/components',
+			'$components/*': 'src/components/*'
+		}
 	},
 	vitePlugin: {
 		experimental: {
