fc5bde5051c9a0fa2d7f2426e3e4939c3df6edf4
VICTORVICKIE <s.vickie14@gmail.com>
2023-01-28 12:31:15 +0530

    Added: Product Card & Quantity Counter Component

---
+++ src/lib/components/ProductCard.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import ThemeToggleButton from '$lib/components/ThemeToggleButton.svelte'
+	import type { Product } from '$lib/types/Product'
+	import QuantityCounter from './QuantityCounter.svelte'
+	export let product: Product
+
+	let quantity: number = 1
+	$: price = product.price * quantity
+</script>
+
+<div
+	class="card mx-auto mt-12 w-80 bg-base-100 shadow-sm shadow-[#808080] transition-all hover:scale-[101%] hover:shadow-md hover:shadow-zinc-800 "
+>
+	<div class="mx-8 justify-between card-title my-4">
+		<h2>{product.title}</h2>
+		<div class="badge">{product.category}</div>
+	</div>
+	<figure class="px-8">
+		<img src={product.image} alt="Shoes" class="rounded-md" />
+	</figure>
+	<div class="card-body gap-4">
+		<div class="card-actions items-center justify-between">
+			<div class="flex">
+				<iconify-icon class="my-auto" icon="ph:currency-inr-bold" />{price}
+			</div>
+			<QuantityCounter bind:quantity />
+		</div>
+		<div class="card-actions">
+			<button class="btn flex-1 rounded-md btn-primary">Add to List</button>
+			<ThemeToggleButton />
+		</div>
+	</div>
+</div>
---
+++ src/lib/components/QuantityCounter.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { numeric } from '$lib/validate/Numeric'
+
+	export let quantity = 1
+</script>
+
+<div class="flex flex-row h-10 w-fit rounded-md">
+	<button
+		data-action="decrement"
+		class=" h-full w-8 rounded-l-md cursor-pointer outline-none"
+		disabled={quantity <= 1}
+		on:click={() => quantity--}
+	>
+		<span class="m-auto text-2xl font-thin">−</span>
+	</button>
+	<input
+		type="text"
+		inputmode="numeric"
+		pattern="\d*"
+		class="input input-bordered text-center h-full w-16"
+		name="custom-input-number"
+		value={quantity}
+		on:keypress={numeric}
+	/>
+	<button
+		data-action="increment"
+		class=" h-full w-8 rounded-r-md cursor-pointer"
+		on:click={() => quantity++}
+	>
+		<span class="m-auto text-2xl font-thin">+</span>
+	</button>
+</div>
