54e84adfde409da983d8bd79471a6cbd7fc3d6a6
VICTORVICKIE <s.vickie14@gmail.com>
2023-01-28 12:25:11 +0530

    Refactor: Dark & Light mapped to Black & Lofi

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,6 +15,23 @@ module.exports = {
 		logs: true,
 		rtl: false,
 		prefix: '',
-		themes: ['black', 'lofi']
+		themes: [
+			{
+				dark: {
+					...require('daisyui/src/colors/themes')['[data-theme=black]'],
+					'--btn-text-case': 'uppercase',
+					'--rounded-box': '0.25rem',
+					'--rounded-btn': '0.125rem',
+					'--rounded-badge': '0.125rem',
+					'--animation-btn': '0',
+					'--animation-input': '0',
+					'--btn-focus-scale': '1',
+					'--tab-radius': '0'
+				},
+				light: {
+					...require('daisyui/src/colors/themes')['[data-theme=lofi]']
+				}
+			}
+		]
 	}
 }
