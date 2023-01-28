c6c0f7dbe62ec70d5915fa7da4feef87f7238a63
VICTORVICKIE <s.vickie14@gmail.com>
2023-01-28 12:23:10 +0530

    Refactor: Moved Component inside lib

--- src/lib/components/ThemeToggleButton.svelte
+++ src/lib/components/ThemeToggleButton.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import { browser } from '$app/environment'
 	// default theme
-	var theme: string = browser ? document.cookie.split('=')[1] ?? 'black' : 'black'
+	var theme: string = browser ? document.cookie.split('=')[1] ?? 'dark' : 'dark'
 
 	function set_theme() {
-		theme = theme === 'black' ? 'lofi' : 'black'
+		theme = theme === 'dark' ? 'light' : 'dark'
 
 		document.documentElement.dataset.theme = theme
 		document.cookie = `se_theme=${theme};max-age=31536000;path="/"`
@@ -14,7 +14,7 @@
 <!-- svelte-ignore a11y-label-has-associated-control -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <label
-	class="swap pl-4 pt-2 justify-start swap-rotate {theme === 'black' ? '' : 'swap-active'}"
+	class="swap pl-4 pt-2 justify-start swap-rotate {theme === 'dark' ? '' : 'swap-active'}"
 	on:click={set_theme}
 >
 	<iconify-icon class="swap-on" icon="ph:sun-duotone" width="24" height="24" />
