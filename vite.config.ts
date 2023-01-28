69dcd4582b263dc639c5ae8c9abd397f6eaab66b
VICTORVICKIE <s.vickie14@gmail.com>
2023-01-28 12:32:18 +0530

    Refactor: Auto Format

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { multicssclass } from 'svelte-multicssclass'
 import type { UserConfig } from 'vite'
+
 const config: UserConfig = {
 	plugins: [
 		// @ts-ignore
 		multicssclass(),
-		sveltekit()
+		sveltekit(),
 	]
 }
 
