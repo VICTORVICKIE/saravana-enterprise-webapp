cfe2c25ca2e13eca2facbd547854fc25c3dd0c77
VICTORVICKIE <s.vickie14@gmail.com>
2023-01-30 17:16:26 +0530

    Added: Icons

--- vite.config.ts
+++ vite.config.ts
@@ -12,12 +12,29 @@ const config: UserConfig = {
 			includeAssets: ['fonts/*.*', 'images/*.png'],
 			registerType: 'autoUpdate',
 			manifest: {
-				name: 'Saravana Enterprise App',
-				short_name: 'Saravana ENT',
+				name: 'Saravana Enterprise',
+				short_name: 'Saravana Enterprise',
 				description: 'Saravana Enterprise Order App',
 				theme_color: '#000000',
 				display: 'standalone',
-
+				icons: [
+					{
+						src: 'icons/android-chrome-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'icons/android-chrome-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					},
+					{
+						"src": "icons/maskable_icon.png",
+						"sizes": "196x196",
+						"type": "image/png",
+						"purpose": "any maskable"
+					}
+				]
 			}
 		})
 	]
