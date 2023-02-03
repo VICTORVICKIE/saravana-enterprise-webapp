eb22a187372568e0f471ea6c0929502492d521eb
VICTORVICKIE <s.vickie14@gmail.com>
2023-02-03 08:44:28 +0530

    Removed: Tailwind-Scrollbar-Hide

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,6 @@ const { default: themes } = require('daisyui/src/colors/themes')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,svelte,ts}'],
-	purge: ['./src/**/*.{html,svelte,ts}'],
 	theme: {
 		extend: {
 			gridTemplateColumns: {
@@ -11,7 +10,7 @@ module.exports = {
 			}
 		}
 	},
-	plugins: [require('daisyui'), require('tailwind-scrollbar-hide')],
+	plugins: [require('daisyui')],
 	daisyui: {
 		styled: true,
 		themes: true,
