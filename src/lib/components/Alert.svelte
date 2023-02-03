a3feab0a57dde4543f69f59394f20da018cfd400
VICTORVICKIE <s.vickie14@gmail.com>
2023-02-03 08:52:51 +0530

    Added: Alert Comp for later use in form validation

---
+++ src/lib/components/Alert.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let content: string
+</script>
+
+<div class="alert justify-center bg-warning-content shadow-md shadow-neutral">
+	<div>
+		<iconify-icon icon="ph:warning-duotone" class="text-warning" width="24" height="24" />
+		<span>{content}</span>
+	</div>
+</div>
