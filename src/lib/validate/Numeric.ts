c621f96a2aa303b45a270a227475b096423ef85d
VICTORVICKIE <s.vickie14@gmail.com>
2023-02-03 08:53:37 +0530

    Added: Helper event , validate form in client side

--- src/lib/validate/Numeric.ts
+++ src/lib/validate/Numeric.ts
@@ -1,6 +1,17 @@
-export function numeric(event: { key: string; preventDefault: () => void }) {
-	// custom logic for handling input
+export function numeric(event: KeyboardEvent) {
+	let input = event.target as HTMLInputElement
+
+	switch (input.name) {
+		case "phone":
+
+			break;
+
+		default:
+			break;
+	}
+
 	if (!/^\d+$/.test(event.key) && event.key !== 'Enter') {
 		event.preventDefault()
 	}
 }
+
