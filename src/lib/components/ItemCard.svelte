16983e8a03614bc79485d2207ed9d190dd841e5a
VICTORVICKIE <s.vickie14@gmail.com>
2023-02-06 19:38:05 +0530

    Added: Component for Enterprise Pages

---
+++ src/lib/components/ItemCard.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { Item } from '$lib/types'
+
+	export let item: Item
+</script>
+
+<tr class="hover h-28 hover:cursor-pointer">
+	<td><img src={item.image_url} class="inline h-24 w-24" />{item.name}</td>
+	<td class="text-center">₹{item.price}</td>
+	<td class="text-center">x {item.quantity}</td>
+	<td class="text-center">₹{item.subtotal}</td>
+</tr>
---
+++ src/lib/components/OrderCard.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { goto } from '$app/navigation'
+	import { StateColors, States, type State } from '$lib/constants'
+	import type { Order } from '$lib/types'
+
+	export let order: Order
+
+	export let value: number = States.indexOf(order.state as State)
+
+	const submit_state = async () => {
+		const res = await fetch('/api/orders', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(data)
+		})
+	}
+	const goto_order = () => goto(`/enterprise/orders/${order.id}`)
+
+	$: text = States[value]
+	$: color = StateColors[value]
+
+	let order_id: number = order.id
+	$: data = { order_id, value }
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<tr class="hover hover:cursor-pointer">
+	<td on:click={goto_order}>{order.id}</td>
+	<td on:click={goto_order}>{order.user.name}</td>
+	<td on:click={goto_order}>₹{order.total}</td>
+	<td on:click={goto_order}><div class="badge-{color} badge w-24 gap-2">{text}</div></td>
+	<td>
+		<input
+			on:change={submit_state}
+			type="range"
+			min="1"
+			max="3"
+			bind:value
+			class="range range-{color} w-24"
+			step="1"
+		/>
+	</td>
+</tr>
