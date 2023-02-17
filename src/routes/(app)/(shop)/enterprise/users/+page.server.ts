eda9367e4d7fbe8c12a17fe1d816ec814bbef6f1
VICTORVICKIE <s.vickie14@gmail.com>
2023-02-17 17:36:27 +0530

    Added: Enterprise User Page

---
+++ src/routes/(app)/(shop)/enterprise/users/+page.server.ts
@@ -0,0 +1,18 @@
+import { SECRET_INTERNAL_API_KEY } from '$env/static/private'
+import type { User } from '$lib/types'
+import type { PageServerLoad } from './$types'
+
+export const load: PageServerLoad = async ({ fetch }) => {
+    const get_users = async () => {
+        const res = await fetch('/api/users', {
+            headers: {
+                'Authorization': `Bearer ${SECRET_INTERNAL_API_KEY}`
+            }
+        })
+        const data: User[] = await res.json()
+
+        return data
+    }
+
+    return { users: get_users() }
+}
