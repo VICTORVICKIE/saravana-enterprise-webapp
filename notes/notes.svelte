60572e373ccc734267332c075c423a6721bba092
VICTORVICKIE <s.vickie14@gmail.com>
2023-02-17 17:44:14 +0530

    Added: Notes for project, will be removed later

---
+++ notes/notes.svelte
@@ -0,0 +1,26 @@
+<script>
+	function handleInput(evt) {
+		// custom logic for handling input
+		if (!/^\d+$/.test(evt.key) && evt.key !== 'Enter') {
+			evt.preventDefault()
+		}
+	}
+</script>
+
+<input type="password" on:keypress={handleInput} />
+
+<h1 class="text-3xl font-bold {false ? 'text-green-700' : 'text-red-700'}">Hello world!</h1>
+<h1 class:text-green-700;underline={true} class="text-3xl font-bold ">Hello world!</h1>
+
+@apply transition-all hover:scale-[101%] hover:shadow-xl;
+
+<link rel="apple-touch-icon" sizes="180x180" href="icons/apple-touch-icon.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png" />
+<link rel="mask-icon" href="icons/safari-pinned-tab.svg" color="#5bbad5" />
+<meta name="msapplication-TileColor" content="#2b5797" />
+<meta name="theme-color" content="#000000" />
+
+Parlay: $5: Red Green Yellow Black Fulltoss: Red Green Yellow Orange Namkin: Moong Dhal Khatta Mitta
+Hot 'n' Spicy Aloo Buja Masala Peanut Crax: curls fritz cheese ball pasta noodles Gift rings Too
+Yum: Green Black Red Yellow Blue Karare: Green Blue Orange
---
+++ src/routes/(app)/feature/+page.server.ts
@@ -0,0 +1,8 @@
+import type { Action, Actions } from '@sveltejs/kit'
+
+const feature: Action = async ({ cookies, request }) => {
+	const form_data: FormData = await request.formData()
+	console.log(form_data)
+}
+
+export const actions: Actions = { feature }
