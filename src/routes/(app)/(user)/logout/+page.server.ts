a6d5657111be4e5debf6f76dc02b1a8766f12dad
VICTORVICKIE <s.vickie14@gmail.com>
2023-02-23 18:35:29 +0530

    fix: logout

--- src/routes/(app)/(user)/logout/+page.server.ts
+++ src/routes/(app)/(user)/logout/+page.server.ts
@@ -1,17 +1,18 @@
+import { Roles } from '$lib/constants'
 import { redirect } from '@sveltejs/kit'
 import type { Actions, PageServerLoad } from './$types'
 
-export const load: PageServerLoad = async () => {
-	throw redirect(302, '/')
-}
+// export const load: PageServerLoad = async () => {
+// 	throw redirect(302, '/')
+// }
 
 export const actions: Actions = {
-	async default({ cookies }) {
-		const session = cookies.get('session')
-		if (!session) {
+	async default({ cookies, locals }) {
+		if (locals.user.role === Roles[0]) {
 			throw redirect(302, '/login')
 		}
 
+		const session = cookies.get('session')
 		await prisma.session.delete({ where: { id: session } })
 
 		cookies.delete('session', {
