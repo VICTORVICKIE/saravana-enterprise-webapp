e53469eb1d7f2fe2a77134e878aff12715fb2ce1
VICTORVICKIE <s.vickie14@gmail.com>
2023-02-23 18:49:42 +0530

    i m dumb

--- src/routes/(app)/(user)/logout/+page.server.ts
+++ src/routes/(app)/(user)/logout/+page.server.ts
@@ -1,10 +1,7 @@
 import { Roles } from '$lib/constants'
+import { prisma } from '$lib/server/prisma'
 import { redirect } from '@sveltejs/kit'
 import type { Actions, PageServerLoad } from './$types'
-
-// export const load: PageServerLoad = async () => {
-// 	throw redirect(302, '/')
-// }
 
 export const actions: Actions = {
 	async default({ cookies, locals }) {
@@ -13,9 +10,7 @@ export const actions: Actions = {
 		}
 
 		const session = cookies.get('session')
-		console.log(session)
 		await prisma.session.delete({ where: { id: session } })
-		console.log("after")
 		cookies.delete('session', {
 			path: '/'
 		})
